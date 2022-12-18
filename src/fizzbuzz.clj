57af439a18537f1e0dcfe0565d597acb0597550f
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-12-18 11:54:13 +0900

    fix: fizzbuzz and add deps

--- src/fizzbuzz.clj
+++ src/fizzbuzz.clj
@@ -1,12 +1,24 @@
 (ns fizzbuzz)
 
-(defn fizzbuzz [n]
+;; ループ回数
+(def iter 50)
+
+;; cond
+(defn fizzbuzz-cond [n]
   (cond
     (= (mod n 15) 0) "FizzBuzz"
     (= (mod n  5) 0) "Buzz"
     (= (mod n  3) 0) "Fizz"
     :else n))
 
-;; FIXME: ぬるぽ(ｶﾞｯ
-((dotimes [i 50]
-   (println (fizzbuzz i))))
+(for [x (range 1 iter)]
+  (fizzbuzz-cond x))
+
+;; if
+(defn fizzbuzz-if [x]
+  (if (= (mod x 15) 0) "FizzBuzz"
+      (if (= (mod x 5) 0) "Buzz"
+          (if (= (mod x 3) 0) "Fizz" x))))
+
+(for [x (range 1 iter)]
+  (fizzbuzz-if x))
